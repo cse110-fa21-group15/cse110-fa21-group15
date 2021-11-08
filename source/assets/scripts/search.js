4c39bda094cfbd9b59f10d9c6d6e9662b0edaf7c
Xiaoye <x5zuo@ucsd.edu>
2021-11-07 22:23:07 -0800

    Added another recipe call method

--- source/assets/scripts/search.js
+++ source/assets/scripts/search.js
@@ -1,24 +1,41 @@
+const API_KEY = "d97a6e5c0e914bed85a619b32c3aa047";
+
 //search recipes
 document.getElementById('search').addEventListener("click", getRecipe);
 
-function getSource(id){
-    let input = document.getElementById('search').value;
-    $.ajax({
-        url:"https://api.spoonacular.com/recipes/" + id +"/information?apiKey=d97a6e5c0e914bed85a619b32c3aa047",
-        success:function(response){
-            document.getElementById("sourceLink").innerHTML=response.sourceUrl;
-            document.getElementById("sourceLink").href=response.sourceUrl;
+async getRecipe(){
+    try{
+        await fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=d97a6e5c0e914bed85a619b32c3aa047&query='+input)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById('img-output').src=""
+        });
+    }
+        catch(error){
+            console.log(error);
         }
-    })
+    }
 }
 
-function getRecipe(){
-    let input = document.getElementById('search').value;
-    $.ajax({
-        url:"https://api.spoonacular.com/recipes/complexSearch?apiKey=d97a6e5c0e914bed85a619b32c3aa047&query="+input,
-        success:function(response){
-            document.getElementById("img-output").src=response.baseUrl+response.results[o].image;
-            getSource(response.results[0].id);
-        }
-    })
-}
+// function getSource(id){
+//     let input = document.getElementById('search').value;
+//     $.ajax({
+//         url:"https://api.spoonacular.com/recipes/" + id +"/information?apiKey=d97a6e5c0e914bed85a619b32c3aa047",
+//         success:function(response){
+//             document.getElementById("sourceLink").innerHTML=response.sourceUrl;
+//             //document.getElementById("sourceLink").href=response.sourceUrl;
+//         }
+//     })
+// }
+
+// function getRecipe(){
+//     let input = document.getElementById('search').value;
+//     $.ajax({
+//         url:"https://api.spoonacular.com/recipes/complexSearch?apiKey=d97a6e5c0e914bed85a619b32c3aa047&query="+input,
+//         success:function(response){
+//             console.log(response.baseUrl);
+//             document.getElementById("img-output").src=response.baseUrl+response.results[o].image;
+//             //getSource(response.results[0].id);
+//         }
+//     })
+// }
