c030d8de42ed6d0905eb93a4c5eb176197e8dfb3
Naweed Malal <naweedmalal@gmail.com>
2021-11-17 20:42:30 -0800

    Added recipe cards to cookbook

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -1,13 +1,162 @@
 // main.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.3.0/firebase-auth.js'
+import { getFirestore, collection, addDoc, query, where, getDocs, getDoc, updateDoc, arrayUnion, doc, arrayRemove } from 'https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js'
+
 
 // Here is where the recipes that you will fetch.
 // Feel free to add your own here for part 2, if they are local files simply add their path as a string.
-const recipes = [
-    //'https://introweb.tech/assets/json/ghostCookies.json',
-    //'https://introweb.tech/assets/json/birthdayCake.json',
-    //'https://introweb.tech/assets/json/chocolateChip.json'
-    'assets/recipes/test.json'
-  ];
+const firebaseConfig = {
+  apiKey: "AIzaSyCwaLuRVV073aNbTB5EaLoZDIFXGzvqr3A",
+  authDomain: "easy-chef-3eb03.firebaseapp.com",
+  projectId: "easy-chef-3eb03",
+  storageBucket: "easy-chef-3eb03.appspot.com",
+  messagingSenderId: "744097831580",
+  appId: "1:744097831580:web:ef9a05d277c2b1785b2b59",
+  measurementId: "G-JKV8H3SLTR"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+// UNCOMMENT THE 2 BELOW LINES WHEN BUTTONS ARE CONNECTED TO SIGNIN AND SIGNUP
+
+// document.querySelector('#signUp').addEventListener('click', signUp);
+// document.querySelector('#signIn').addEventListener('click', signIn);
+/**
+ * Signup function that creates new user and returns the user id
+ */
+async function signUp() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var user_id;
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    user_id = user.uid;
+    addUser(email, user.uid);
+    const userInformation = getUser(user.uid);
+    console.log(userInformation);
+    return userInformation;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}
+
+/**
+ * Sign in function that returns a user ID
+ */
+async function signIn() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  console.log(email);
+  console.log(password);
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      // UID specifies which user we are talking about
+      const userInformation = getUser(user.uid);
+      return userInformation;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+}
+
+/**
+ * addUser function that adds a user to the FireStore Database after creating an account
+ * @param {string} email 
+ * @param {string} id 
+ */
+async function addUser(email, id) {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      user_email: email,
+      user_id: id,
+      favoriteRecipes: [],
+    });
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+
+// DELETE COMMENT WHEN DONE! ADD PARAMETERS!!
+/**
+ * 
+ * @param {id of user logged in} id 
+ * @param {name of recipe} name
+ * @param {Time it takes to cook the meal} time 
+ * @param {Cost of ingredients in the meal} cost 
+ * @param {Number of servings in the meal} servings 
+ * @param {Information regarding the meal} description 
+ * @param {Image of meal} image
+ * @param {tag of meal} tag
+ */
+ async function createRecipe(name, time, cost, servings, description) {
+   let id = "D3TKWTnCklTvt5dWDNPlLbUQYa53"
+  const q = query(collection(db, "users"), where("user_id", "==", id));
+  const querySnapshot = await getDocs(q);
+  const document = querySnapshot.docs[0];
+  console.log(document.id)
+  console.log(document)
+  const database = doc(db, "users", document.id);
+  // console.log(document.data())
+    await updateDoc(database, {
+      favoriteRecipes: arrayUnion({name: name, time: time, cost: cost, servings: servings, description: description})
+    })
+}
+
+async function removeRecipe() {
+  let id = "D3TKWTnCklTvt5dWDNPlLbUQYa53"
+  const q = query(collection(db, "users"), where("user_id", "==", id));
+  const querySnapshot = await getDocs(q);
+  const document = querySnapshot.docs[0];
+  console.log(document.id)
+  console.log(document)
+  let name = "pizza";
+  let time = "30";
+  let cost = "40";
+  let servings = "3";
+  let description = "testing";
+  const database = doc(db, "users", document.id);
+  // console.log(document.data())
+    await updateDoc(database, {
+      favoriteRecipes: arrayRemove({name: "pizza"})
+    })
+}
+
+//document.querySelector('#tester').addEventListener('click', removeRecipe)
+
+/**
+ * Returns the information of a signed user such as favorite recipes, email, ID
+ * @param {String} id 
+ * @returns information regarding the user
+ */
+async function getUser() {
+  const id = "D3TKWTnCklTvt5dWDNPlLbUQYa53";
+  console.log("TEST");
+  const userInformation = {};
+  const users = collection(db, "users");
+  const q = await query(users, where("user_id", "==", id));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    userInformation["results"] = doc.data();
+  });
+  return userInformation;
+}
+
+/* main.js STARTS HERE */
+
+//Get users' favorite recipes
+const user = await getUser();
+
+const recipes = user["results"].favoriteRecipes;
 
   let numRecipes;
   
@@ -16,7 +165,8 @@ const recipes = [
   // keys as long as it's unique, one suggestion might but the URL itself
   const recipeData = {}
   
-  window.addEventListener('DOMContentLoaded', init);
+  //Call this to begin getting recipe cards
+  init();
   
   // This is the first function to be called, so when you are tracing your code start here.
   async function init() {
@@ -29,11 +179,9 @@ const recipes = [
     };
     // Add the first three recipe cards to the page
     createRecipeCards();
-    console.log("test");
     // Make the "Show more" button functional
     bindShowMore();
   }
-  
   async function fetchRecipes() {
     return new Promise((resolve, reject) => {
       // This function is called for you up above
@@ -47,22 +195,16 @@ const recipes = [
       // in the recipes folder and fetch them from there. You'll need to add their paths to the recipes array.
   
       // Part 1 Expose - TODO
-      
-      let stringNum = "";
-        fetch(recipes[0])
-        .then(response => response.json())
-        .then(function(data){
-          numRecipes = searchForKey(data, "number");
-          for(let i = 0; i < numRecipes; ++i){
-            stringNum = i.toString();
-            recipeData[stringNum] = data["results"][i];
-            if(i == numRecipes - 1){
-              resolve(true);
-            }
+        numRecipes = recipes.length;
+
+        console.log(recipes);
+        //Parse recipes from JSON to recipeData
+        for(let i = 0; i < numRecipes; ++i){
+          recipeData[i] = recipes[i];
+          if(i == numRecipes - 1){
+            resolve(true);
           }
-        })
-        .catch(() => reject(false));
-      
+        }
     });
   }
   
@@ -114,4 +256,8 @@ const recipes = [
       });
       return value;
     }
-    
+
+
+
+
+// document.querySelector('#add').addEventListener('click', getUser)
