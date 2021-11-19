bb4634924ae77e3ea69e0c5fd630866a27938b25
Naweed Malal <naweedmalal@gmail.com>
2021-11-19 03:17:20 -0800

    selected the text boxes

---
+++ source/assets/scripts/editRecipe.js
@@ -0,0 +1,13 @@
+//editRecipe.js
+
+const recipeName = document.querySelector(".recipeNameText");
+const recipeImage = document.querySelector(".uploadImage");
+const recipeTime = document.querySelector(".timeBoxInput");
+const recipeCost = document.querySelector(".costBoxInput");
+const recipeServings = document.querySelector(".servingsBoxInput");
+const recipeDescription = document.querySelector("#descriptionTextBox");
+const recipeIngredients = document.querySelector("#ingredientsTextBox");
+const recipeSteps = document.querySelector("#stepsTextBox");
+
+
+
