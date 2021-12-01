b2f3b1e3f56f1d33935c2feb9ebfc15b9fb9335b
Sahil Bhalla <s2bhalla@ucsd.edu>
2021-12-01 11:27:09 -0800

    Backend Search really done

--- source/assets/scripts/search.js
+++ source/assets/scripts/search.js
@@ -23,15 +23,15 @@ async function getRecipes(event, filters = false, number = 8, offset = 0, currsi
         var dietary = document.getElementById("dietary").value;
         var cost = document.getElementById("cost").value;
         if(time != ""){
-            //console.log("There is a time filter =" + time)
+            console.log("There is a time filter =" + time)
             url+= ("&maxReadyTime=" + time);
         }
         if(dietary != ""){
-            //console.log("There is a dietary filter =" + dietary)
+            console.log("There is a dietary filter =" + dietary)
             url+= ("&diet=" + dietary);
         }
         if(cost != ""){
-            //console.log("There is a cost filter =" + cost)
+            console.log("There is a cost filter =" + cost)
             url+= "&addRecipeInformation=true";
         }
 
