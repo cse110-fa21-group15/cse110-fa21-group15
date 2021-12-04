344580fa792629215a71973e8f95ca763ef9a888
DarylFSN <44512648+DarylFSN@users.noreply.github.com>
2021-12-03 20:06:29 -0800

    Format Fix

--- source/assets/scripts/deleteRecipe.js
+++ source/assets/scripts/deleteRecipe.js
@@ -4,17 +4,16 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { getFirestore, collection, addDoc, query, where, getDocs, getDoc, updateDoc, arrayUnion, doc, arrayRemove, deleteDoc } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
 import { firebaseConfig } from './api.js';
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth();
-  const db = getFirestore();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
 
 /**
  * Deletes recipe from database
  * @param {string} recipe_id ID of recipe to be deleted
  */
-async function deleteRecipe(recipe_id, user_id) {
-    
-    //Deletes document from recipes table
+async function deleteRecipe(recipe_id, user_id) {  
+    // Deletes document from recipes table
     await deleteDoc(doc(db, "recipes", recipe_id));
     const q = query(collection(db, "users"), where("user_id", "==", user_id));
     const querySnapshot = await getDocs(q);
@@ -22,8 +21,8 @@ async function deleteRecipe(recipe_id, user_id) {
     const database = doc(db, "users", documents.id);
     await updateDoc(database, {
         favoriteRecipes: arrayRemove(recipe_id)
-      });
-      location.href = "cookbook.html";
+    });
+    location.href = "cookbook.html";
 }
 
 /**
@@ -33,21 +32,18 @@ async function deleteRecipe(recipe_id, user_id) {
  * @param {ID of current user} user_id 
  */
 async function favoriteRecipe(recipe_id, user_id) {
-  const database = doc(db, "users", user_id);
-  try {
-  await updateDoc(database, {
-    favorites: arrayUnion(recipe_id)
-  })
-  } catch (e) {
-  console.error("Error adding favorite recipe")
-  }
+const database = doc(db, "users", user_id);
+    try {
+        await updateDoc(database, {
+            favorites: arrayUnion(recipe_id)
+        })
+    } catch (e) {
+        console.error("Error adding favorite recipe")
+    }
 }
 
-
 const deleteBtn = document.querySelector("#delete");
-
 const recipe = JSON.parse(localStorage.recipe);
-
 const user_id = searchForKey(recipe, "user_id");
 const recipe_id = searchForKey(recipe, "recipe_id");
 
@@ -55,7 +51,7 @@ const recipe_id = searchForKey(recipe, "recipe_id");
 //console.log(user_id);
 //console.log(recipe_id);
 
-deleteBtn.addEventListener("click", function(){
+deleteBtn.addEventListener("click", function() {
     //console.log("onlick is working");
     deleteRecipe(recipe_id, user_id);
 })
@@ -68,17 +64,16 @@ deleteBtn.addEventListener("click", function(){
  * @returns {*} the value of the found key
  */
 function searchForKey(object, key) {
-  var value;
-  Object.keys(object).some(function (k) {
-    if (k === key) {
-      value = object[k];
-      return true;
-    }
-    if (object[k] && typeof object[k] === "object") {
-      value = searchForKey(object[k], key);
-      return value !== undefined;
-    }
-  });
-  return value;
+    let value;
+    Object.keys(object).some(function (k) {
+        if (k === key) {
+            value = object[k];
+            return true;
+        }
+        if (object[k] && typeof object[k] === "object") {
+            value = searchForKey(object[k], key);
+            return value !== undefined;
+        }
+    });
+    return value;
 }
-
