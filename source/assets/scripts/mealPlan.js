e228df24894888f02a116ad80e4d272340c5db5a
Sahil Bhalla <s2bhalla@ucsd.edu>
2021-12-04 21:09:22 -0800

    Co-authored-by: Lavanya Verma <FiendFyre5@users.noreply.github.com> Co-authored-by: Xiaoye Zuo <LavitaZ@users.noreply.github.com>

--- source/assets/scripts/mealPlan.js
+++ source/assets/scripts/mealPlan.js
@@ -1,13 +1,53 @@
 function allowDrop(ev) {
     ev.preventDefault();
-  }
-  
-  function drag(ev) {
+}
+
+function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
+}
+
+function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-  }
+}
+
+function init(){
+    var data = getUser();
+    console.log(data)
+}
+
+init()
+
+/**
+ * Create recipe cards to be displayed 
+ */
+function createRecipeCards() {
+    let parentDiv = document.querySelector(".parentDiv");
+    let mainElement = document.querySelector("main");
+    for (let i = 0; i < numRecipes; i++) {
+        let recipeCard = document.createElement("recipe-card");
+        recipeCard.data = recipeData[i.toString()];
+        parentDiv.appendChild(recipeCard);
+    }
+    mainElement.appendChild(parentDiv);
+}
+
+/**
+ * Fetch recipes and populate them into recipeData
+ * @param {Array} recipes recipes to fetch
+ */
+ async function fetchRecipes(recipes) {
+    return new Promise((resolve, reject) => {  
+        numRecipes = recipes.length;
+        console.log(recipes);
+        // Parse recipes from JSON to recipeData
+        for (let i = 0; i < numRecipes; i++) {
+            recipeData[i] = recipes[i];
+            if(i == numRecipes - 1) {
+                resolve(true);
+            }
+        }
+    });
+}
+
