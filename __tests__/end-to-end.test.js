6d357fc392a28cf37e9585163fa48b8c981f67c7
Jonathan Padungyothee <jpadungy@ucsd.edu>
2021-12-09 01:33:36 -0800

    trial test

--- __tests__/end-to-end.test.js
+++ __tests__/end-to-end.test.js
@@ -79,41 +79,41 @@ describe("Test Signup and Login", () => {
         expect(ddlist.length).toBe(15);
     }, 10000);
 
-    // it("checking if creating recipes works", async () => {
-    //     const browser = await puppeteer.launch({
-    //         slowMo:100,
-    //         headless:true,
-    //         defaultViewport: {
-    //             width:1280,
-    //             height:720
-    //         }
-    //     });
-    //     const page = await browser.newPage();
-    //     await page.goto("https://festive-minsky-ab51a6.netlify.app/source/signin");
-    //     const ebar = await page.$eval("#email", (e) => e.value = "bruh@gmail.com");
-    //     const pbar = await page.$eval("#password", (e) => e.value = "testpassword");
-    //     const button = await page.$("#lbutton");
-    //     await button.click();
-    //     await page.waitForNavigation();
-    //     const drop = await page.$("button");
-    //     await drop.click();
-    //     const crecipe = await page.$("#recipeUpload");
-    //     await crecipe.click();
-    //     await page.waitForNavigation();
-    //     const input = await page.$eval(".recipeNameText", (e) => e.value = "Omelette");
-    //     const uploader = await page.$("#imageUpload");
-    //     await uploader.uploadFile("/__tests__/img.jpeg");
+    it("checking if creating recipes works", async () => {
+        const browser = await puppeteer.launch({
+            slowMo:100,
+            headless:true,
+            defaultViewport: {
+                width:1280,
+                height:720
+            }
+        });
+        const page = await browser.newPage();
+        await page.goto("https://festive-minsky-ab51a6.netlify.app/source/signin");
+        const ebar = await page.$eval("#email", (e) => e.value = "bruh@gmail.com");
+        const pbar = await page.$eval("#password", (e) => e.value = "testpassword");
+        const button = await page.$("#lbutton");
+        await button.click();
+        await page.waitForNavigation();
+        const drop = await page.$("button");
+        await drop.click();
+        const crecipe = await page.$("#recipeUpload");
+        await crecipe.click();
+        await page.waitForNavigation();
+        const input = await page.$eval(".recipeNameText", (e) => e.value = "Omelette");
+        const uploader = await page.$("#imageUpload");
+        await uploader.uploadFile("/__tests__/img.jpeg");
 
-    //     const time = await page.$eval(".timeBoxInput", (e) => e.value = "10 mins");
-    //     const cost = await page.$eval(".costBoxInput", (e) => e.value = "$5");
-    //     const size = await page.$eval(".servingsBoxInput", (e) => e.value = "1 serving");
-    //     const desc = await page.$eval("#descriptionBoxInput", (e) => e.value = "How to make a breakfast omelette!");
-    //     const ings = await page.$eval("#ingredientsBoxInput", (e) => e.value = "eggs, chili powder, salt, oil");
-    //     const steps = await page.$eval("#stepsBoxInput", (e) => e.value = "beat egg in bowl, add chili powder, add salt, add oil to a heated pan, pour egg mixture into the pan");
+        const time = await page.$eval(".timeBoxInput", (e) => e.value = "10 mins");
+        const cost = await page.$eval(".costBoxInput", (e) => e.value = "$5");
+        const size = await page.$eval(".servingsBoxInput", (e) => e.value = "1 serving");
+        const desc = await page.$eval("#descriptionBoxInput", (e) => e.value = "How to make a breakfast omelette!");
+        const ings = await page.$eval("#ingredientsBoxInput", (e) => e.value = "eggs, chili powder, salt, oil");
+        const steps = await page.$eval("#stepsBoxInput", (e) => e.value = "beat egg in bowl, add chili powder, add salt, add oil to a heated pan, pour egg mixture into the pan");
 
-    //     const click = await page.$eval(".btTxt", (e) => e.click());
-    //     await page.waitForNavigation();
-    //     await browser.close();
-    //     expect(page.url()).toBe("https://festive-minsky-ab51a6.netlify.app/source/cookbook.html");
-    // }, 10000);
+        const click = await page.$eval(".btTxt", (e) => e.click());
+        await page.waitForNavigation();
+        await browser.close();
+        expect(page.url()).toBe("https://festive-minsky-ab51a6.netlify.app/source/cookbook.html");
+    }, 10000);
 });
